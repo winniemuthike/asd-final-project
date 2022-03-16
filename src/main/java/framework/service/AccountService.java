ef5b15644acb4af6b4c2ebe82353708357380bb7
Ahmed Fayez <a.fayez707@gmail.com>
2022-03-15 20:16:10 -0500

    Integrating Deposit with the GUI, and handle some exceptions.

--- src/main/java/framework/service/AccountService.java
+++ src/main/java/framework/service/AccountService.java
@@ -9,7 +9,7 @@ public interface AccountService {
     Account saveAccount(Account account);
     Account getAccount(String accountNumber);
     Collection<Account> getAllAccounts();
-    void deposit (String accountNumber, double amount);
+    void deposit (String accountNumber, double amount) throws Exception;
     void withdraw (String accountNumber, double amount);
     void transferFunds(String fromAccountNumber, String toAccountNumber, double amount, String description);
     void addInterest(String accountNumber);
